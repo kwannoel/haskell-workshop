b59fe29a1e10ce0034317a4d9741a51119f13b7c
Kwan Noel <noelkwan1998@gmail.com>
2020-10-15 00:20:47 +0800

    * Setup gitignore
    * Implement interface
    
    * Cleanup project config

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,34 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import           Control.Monad  (forever)
+import           Data.Foldable  (find)
+import           Text.Read      (readMaybe)
+
+import           Game.Go.Launch (initBoard, runGame)
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+    i <- tryExtract isValid getBoardSize
+    let initialBoard = initBoard i
+        game         = runGame initialBoard
+    forever game
+    where
+        -- Tries to get board size
+        getBoardSize = do
+            putStrLn "Enter your board size <X>:"
+            input <- getLine
+            result <- return $ readMaybe input
+            return result
+
+        -- Runs IO action until the extracting function can extract a value
+        tryExtract :: (a -> Maybe b) -> IO a -> IO b
+        tryExtract extract action = do
+            result <- fmap extract action
+            case result of
+                Just validResult -> return validResult
+                _                -> do
+                    putStrLn "Invalid value!\n"
+                    tryExtract extract action
+
+        isValid :: Maybe Int -> Maybe Int
+        isValid = find (> 0)
---
+++ src/Game/Go/Launch.hs
@@ -0,0 +1,7 @@
+-- | Helpers to launch the game
+
+module Game.Go.Launch where
+
+
+initBoard = undefined
+runGame = undefined
