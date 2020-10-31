60d22468ce903934397eef89794bdaa3e0c74d02
Kwan Noel <noelkwan1998@gmail.com>
2020-10-31 13:06:49 +0800

    Publish

--- src/Applicative.hs
+++ src/Applicative.hs
@@ -2,6 +2,8 @@
 
 module Applicative where
 
+import           Prelude hiding (Applicative (..), (**))
+
 {-
 | Law           | Proof                                          |
 |---------------|------------------------------------------------|
@@ -10,6 +12,30 @@ module Applicative where
 | Right id      | u ** unit ≝ u                                  |
 | Associativity | u ** (v ** w) ≝                                |
 -}
+
+class Functor f => Monoidal f where
+    unit :: f ()
+    (**) :: f a -> f b -> f (a, b)
+    (<*>) :: f (a -> b) -> f a -> f b
+
+instance Monoidal [] where
+    unit = [()]
+    [] ** _      = []
+    _ ** []      = []
+    -- all permutations
+    -- xs ** ys = [(x, y) | x <- xs, y <- ys]
+    (x:xs) ** ys = fmap ((,) x) ys <> xs ** ys
+
+--    fs <*> xs = fmap (uncurry ($)) $ fs ** xs
+    fs <*> xs = fmap (\(f, x) -> f x) $ fs ** xs
+
+instance Monoidal Maybe where
+    unit = Just ()
+    Nothing ** _     = Nothing
+    _ ** Nothing     = Nothing
+    Just x ** Just y = Just (x, y)
+
+    Just f <*> Just x = Just $ f x
 
 main :: IO ()
 main = putStrLn "Hello World!"
--- src/Monad.hs
+++ src/Monad.hs
@@ -2,7 +2,6 @@
 
 module Monad where
 
-
 {-
 | Law            | definition `(>>=)`                         | definition `(>=>)`  | definition `join / j`    |
 |----------------|--------------------------------------------|---------------------|--------------------------|
@@ -10,6 +9,41 @@ module Monad where
 | right identity | `m >>= return = m`                         | `f >=> return == f` | `join . fmap pure == id` |
 | associativity  | `(m >>= f) >>= g = m >>= (\x -> fx >>= g)` | `f >=> (g >=> h)`   | `j . fmap j = j . j`     |
 -}
+
+data Product a b = Product a b
+
+instance Functor (Product a) where
+    fmap f (Product a b) = Product a $ f b
+
+instance Monoid a => Applicative (Product a) where
+    pure a = Product mempty a
+    Product s1 f <*> Product s2 a = Product (s1 <> s2) (f a)
+
+-- Composition of writer
+instance Monoid a => Monad (Product a) where
+    return a = Product mempty a
+    Product s1 a1 >>= f = let Product s2 a2 = f a1
+                          in  Product (s1 <> s2) a2
+
+data Reader e a = Reader (e -> a)
+
+instance Functor (Reader e) where
+    fmap f (Reader g) = Reader (f . g)
+
+instance Applicative (Reader e) where
+    pure a = Reader $ \_ -> a
+    Reader f <*> Reader a = Reader $ \e -> let f' = f e
+                                               a' = a e
+                                           in  f' a'
+
+instance Monad (Reader e) where
+    return = pure
+    Reader a >>= g = Reader $ \e -> let a' = a e
+                                        Reader b = g a'
+                                    in b e
+
+
+-- Composition of reader
 
 main :: IO ()
 main = putStrLn "Hello World!"
